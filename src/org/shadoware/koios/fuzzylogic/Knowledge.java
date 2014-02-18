d79b18b6feac35e29b924ebd005aedace4bb5dd4
davidhenar <david.henar@gmail.com>
2014-02-18 19:52:00 +0100

    finished fuzzylogic

--- src/org/shadoware/koios/fuzzylogic/Knowledge.java
+++ src/org/shadoware/koios/fuzzylogic/Knowledge.java
@@ -1,3 +1,10 @@
+/**
+ * Koios. Artificial Inteligence library.
+ * 
+ * @copyright (C) 2014 David Henar Palacin.
+ * @license GNU General Public License version 3 or later, see license.txt
+ */
+
 package org.shadoware.koios.fuzzylogic;
 
 import android.annotation.SuppressLint;
--- src/org/shadoware/koios/fuzzylogic/NoConsecuence.java
+++ src/org/shadoware/koios/fuzzylogic/NoConsecuence.java
@@ -1,5 +1,11 @@
-package org.shadoware.koios.fuzzylogic;
+/**
+ * Koios. Artificial Inteligence library.
+ * 
+ * @copyright (C) 2014 David Henar Palacin.
+ * @license GNU General Public License version 3 or later, see license.txt
+ */
 
+package org.shadoware.koios.fuzzylogic;
 
 
 public class NoConsecuence implements Consecuence {
